70ddbe2f0037882e69d8cafbd8d430a316b9bf58
muhmuhammad_alziqri <muhammad_alziqri@linkaja.id>
2023-06-21 14:16:14 +0700

    feat:
    add new rule
    
    for typescript

--- packages/eslint-config-alziqziq-typescript/index.js
+++ packages/eslint-config-alziqziq-typescript/index.js
@@ -32,5 +32,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/no-inferrable-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
 };
